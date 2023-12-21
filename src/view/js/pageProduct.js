371354409b96ca28823359f8955540171c84bb9e
Carla de Oliveira <carlaolive999@gmail.com>
2023-12-21 15:27:57 -0300

    em desenvolvimento a tela do produto

---
+++ src/view/js/pageProduct.js
@@ -0,0 +1,14 @@
+document.getElementById("btnFavorito").addEventListener('click', (event) => {
+    let info = event.target;
+    let statusFav = info.getAttribute("status")
+
+    if (statusFav == "true") {
+        info.src = "../../../upload/icones/FavoritoFalse.png"
+        let status = "false"
+        info.setAttribute("status", status);
+    } else {
+        info.src = "../../../upload/icones/FavoritoTrue.png"
+        let status = "true"
+        info.setAttribute("status", status);
+    }
+})
