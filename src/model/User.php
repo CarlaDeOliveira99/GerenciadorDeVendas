1db48f977dabea165e6100e769174d26b1ec43f9
Carla de Oliveira <carlaolive999@gmail.com>
2024-04-04 18:31:39 -0300

    correção da sessão para validar user. Botão de sair na tela de gerenciamento de produto

---
+++ src/model/User.php
@@ -0,0 +1,75 @@
+<?php
+namespace src\model;
+use src\model\UserDao;
+class User
+{
+
+	const GESTOR_SIM = 1;
+	const GESTOR_NAO = 0;
+
+	private $nome;
+	private $email;
+	private $cpf; 
+  
+	private $senha;
+	private $gestor;
+	public function validarLogin($emailUser, $senhaUser)
+	{
+		$this->email = $emailUser;
+		$this->senha = $senhaUser;
+		$dao = new UserDao();
+		$result = $dao->validarCadastro($this->email, $this->senha);
+		return $result;
+	}
+	public function userLogado($cpf)
+	{
+		$this->cpf = $cpf;
+		$dao = new UserDao();
+		$result = $dao->verificarUser($this->cpf);
+		return $result;
+	}
+
+
+
+    
+	public function getNome()
+	{
+		return $this->nome;
+	}
+	public function setNome($value)
+	{
+		$this->nome = $value;
+	}
+	public function getCpf()
+	{
+		return $this->cpf;
+	}
+	public function setCpf($value)
+	{
+		$this->cpf = $value;
+	}
+	public function getSenha()
+	{
+		return $this->senha;
+	}
+	public function setSenha($value)
+	{
+		$this->senha = $value;
+	}
+	public function getGestor()
+	{
+		return $this->gestor;
+	}
+	public function setGestor($value)
+	{
+		$this->gestor = $value;
+	}
+	public function getEmail()
+	{
+		return $this->email;
+	}
+	public function setEmail($value)
+	{
+		$this->email = $value;
+	}
+}
